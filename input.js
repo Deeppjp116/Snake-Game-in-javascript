10b7401152d854a57ee4ee82201eb092632486f8
deep <deeppjp1656@gmail.com>
2023-08-09 19:58:32 +0530

    added input file

---
+++ input.js
@@ -0,0 +1,7 @@
+let inputDiraction = { x: 0, y: 0 };
+
+
+export default getInputDireaction(){
+     return inputDiraction 
+}
+ 
--- snake.js
+++ snake.js
@@ -1,16 +1,22 @@
+import { getInputDireaction } from 'input.js';
+
 const SNAKE_SPEED = 1;
 const snakeBody = [
   { x: 10, y: 11 },
   { x: 11, y: 11 },
   { x: 12, y: 11 },
+  { x: 13, y: 11 },
+  { x: 14, y: 11 },
+  { x: 15, y: 11 },
 ];
 
 function updateSnake() {
+  getInputDireaction 
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
-  snakeBody[0].x += 0;
-  snakeBody[0].y += 1 ;
+  // snakeBody[0].x += 0;
+  // snakeBody[0].y += 1;
 }
 
 function createSnakeElement(segment) {
